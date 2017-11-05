c4289ad2db5404522098773323bde0b7e6d1252c
Wojciech <wkrysmann@olx.pl>
2017-11-06 00:25:29 +0100

    working on post.sh

--- helpers/post.sh
+++ helpers/post.sh
@@ -2,23 +2,34 @@
 
 postsDir="../_posts/"
 
+# checking if argument supplied
 if [ $# -ne 1 ]
   then
     echo "Wrong number of arguments supplied."
     exit 1
 fi
 
+
 function findAlbum {
-  local _album=$(ls -1 ../bench/|grep "\-$1\.")
-  echo $_album
+# find album corresponding to given number
+  local _album=`ls -1 ../bench/|grep "\-$1\."`
+
+  if [ -z $_album ]; then
+    echo "No such post!"
+    exit 1
+  else
+    echo $_album
+  fi
 }
 
 function buildName {
+# build new name for release
   local date=`date +%Y-%m-%d`
   echo $date"-"$album
 }
 
 function createPostEntry {
+# copy content from bench to production path
   if [ ! -f $postPath ]; then
     cp ../bench/$album $postPath
   else
@@ -28,8 +39,14 @@ function createPostEntry {
 }
 
 function putDate {
+# add date to file
   local date=`date '+%Y-%m-%d %H:%M:%S'`
-  sed -i -e "s/date:/date: $date/" $postPath
+  sed -i.bck -e "s/date:/date: $date/" $postPath && rm $postPath.bck
+}
+
+function commitToGit {
+  git add $postPath
+  git commit -m "created layout for $1 - $album"
 }
 
 album=$(findAlbum $1)
@@ -37,3 +54,5 @@ postName=$(buildName)
 postPath=$postsDir$postName
 createPostEntry
 putDate
+commitToGit
+atom $postPath
